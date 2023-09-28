6ffdf99968f10e2bf65c9a013eb4fd71a08a8a98
Theo Lorne <tlorne@c2r6p1.42perpignan.fr>
2023-09-28 13:46:35 +0200

    oublie handle

---
+++ handle_dollar.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_dollar.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlorne <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 11:41:38 by tlorne            #+#    #+#             */
+/*   Updated: 2023/09/28 11:42:06 by tlorne           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*int     is_dollar(char *str)
+{
+    int     i;
+    char    fq;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == 34 || str[i] == 39)
+            fq = str[i];
+        
+    }
+}
+
+char	*handle_dollar(char *prompt)
+{
+    char    *fin_prompt;
+
+    if (is_dollar(prompt) == 1)
+        fin_prompt = replace_dol(prompt);
+    else
+        fin_prompt = ft_strdup(prompt);
+    return (fin_prompt);
+}*/

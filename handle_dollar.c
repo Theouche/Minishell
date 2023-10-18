c8dabafa8d86df1838c2d227c33a0304cb49bf4d
Theo Lorne <tlorne@c1r2p5.42perpignan.fr>
2023-10-18 11:20:34 +0200

    tout fonctionne

--- handle_dollar.c
+++ handle_dollar.c
@@ -12,78 +12,87 @@
 
 #include "minishell.h"
 
+int	to_change(char *str, int j)
+{
+	int		i;
+	char	fq;
+
+	i = 0;
+	fq = 0;
+	while (i <= j)
+	{
+		if (str[i] == 34 || str[i] == 39)
+			fq = give_fq(fq, str[i]);
+		else
+		{
+			if (str[i] == 36 && (fq == 0 || fq == 34)
+				&& str[i + 1] && str[i + 1] == '?' && i == j)
+				return (2);
+			if (str[i] == 36 && (fq == 0 || fq == 34)
+				&& str[i + 1] && str[i + 1] != ' '
+				&& str[i + 1] != 0 && str[i + 1] != 34 && i == j)
+				return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+char	*special_case(t_data *data, char *ret)
+{
+	char	*ret2;
+
+	ret2 = ft_strjoin_and_free(ret, ft_itoa(exitstatus(data->status)));
+	return (ret2);
+}
+
 char	*replace_dol_2(t_data *data, char *str, char *ret, int i)
 {
-	(void)data;
-	if (str[i])
+	char	*ret2;
+	int		j;
+
+	data->env_dol = ft_calloc(sizeof(char), 100);
+	j = 0;
+	i++;
+	while (str[i] && (str[i] != ' ' && str[i] != 39 && str[i] != 34))
+		data->env_dol[j++] = str[i++];
+	if (data->env_dol[0] != 0)
+		ret2 = ft_strjoin_and_free(ret, ft_get_env(data, data->env_dol));
+	else
 	{
-		while (str[i])
-		{
-			ret = add_char(ret, str[i]);
-			i++;
-		}
+		ret2 = ft_strdup(ret);
+		free(ret);
 	}
-	return (ret);
+	free(data->env_dol);
+	return (ret2);
 }
 
 char	*replace_dol(t_data *data, char *str)
 {
 	char	*ret;
 	int		i;
-	int		j;
 
-	ret = malloc(sizeof(char) * ft_strlen(str));
+	ret = ft_calloc(sizeof(char), ft_strlen(str));
 	i = 0;
-	while (str[i] != 36)
+	while (str[i])
 	{
-		ret[i] = str[i];
-		i++;
-	}
-	ret[i++] = 0;
-	data->env_dol = malloc(sizeof(char) * 100);
-	j = 0;
-	while (str[i] && (str[i] != ' ' && str[i] != 39 && str[i] != 34))
-		data->env_dol[j++] = str[i++];
-	data->env_dol[j] = 0;
-	ret = ft_strjoin_and_free(ret, ft_get_env(data, data->env_dol));
-	ret = replace_dol_2(data, str, ret, i);
-	free(data->env_dol);
-	return (ret);
-}
-
-char	*special_case_suite(t_data *data, char *str, char *ret, int i)
-{
-	int		len;
-
-	(void)data;
-	len = ft_strlen(str);
-	if (str[i])
-	{
-		while (str[i])
+		while (str[i] && str[i] != 36)
+			ret = add_char(ret, str[i++]);
+		if (str[i + 1] && str[i + 1] == '?' && to_change(str, i))
 		{
-			ret = add_char(ret, str[i]);
-			i++;
+			ret = special_case(data, ret);
+			i += 2;
 		}
+		else if (str[i + 1] && to_change(str, i))
+		{
+			ret = replace_dol_2(data, str, ret, i);
+			i++;
+			while (str[i] && (str[i] != ' ' && str[i] != 39 && str[i] != 34))
+				i++;
+		}
+		else if (str[i])
+			ret = add_char(ret, str[i++]);
 	}
-	return (ret);
-}
-
-char	*special_case(t_data *data, char *str)
-{
-	char	*ret;
-	int		i;
-
-	ret = malloc(sizeof(char) * ft_strlen(str));
-	i = 0;
-	while (str[i] != 36)
-	{
-		ret[i] = str[i];
-		i++;
-	}
-	ret[i++] = 0;
-	ret = ft_strjoin_and_free(ret, ft_itoa(exitstatus(data->status)));
-	i++;
-	ret = special_case_suite(data, str, ret, i);
 	return (ret);
 }
 
@@ -91,10 +100,8 @@ char	*handle_dollar(t_data *data, char *prompt)
 {
 	char	*fin_prompt;
 
-	if (is_dollar(prompt) == 1)
+	if (is_dollar(prompt) == 1 || is_dollar(prompt) == 2)
 		fin_prompt = replace_dol(data, prompt);
-	else if (is_dollar(prompt) == 2)
-		fin_prompt = special_case(data, prompt);
 	else
 		fin_prompt = ft_strdup(prompt);
 	return (fin_prompt);
